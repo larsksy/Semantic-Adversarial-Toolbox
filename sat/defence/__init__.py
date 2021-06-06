43b505e7ca2c83a54005a87dff5f8c6b85721526
Lars Kåre Syversen <larsks@hotmail.no>
2021-06-06 04:05:09 +0200

    minor code cleanup

--- sat/defence/__init__.py
+++ sat/defence/__init__.py
@@ -1,5 +1,4 @@
 from sat.defence.FeatureSqueezing import FeatureSqueezing
 from sat.defence.AdversarialTraining import AdversarialTraining
 from sat.defence.JPEGCompression import JPEGCompression
-from sat.defence.LabelSmoothing import LabelSmoothing
 from sat.defence.Defence import ProcessingDefence
